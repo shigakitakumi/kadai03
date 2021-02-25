5d43ca07cf87df7cbf037a06afa15d5c846521d7
takumi <free.swim.0723@icloud.com>
2021-02-25 10:39:19 +0000

    課題の再提出

--- kadai03.php
+++ kadai03.php
@@ -1,14 +1,18 @@
 <?php
-$name = 紫垣;
+$name = "紫垣";
 if ($name === '紫垣') {
     echo "私は紫垣です。";
-}   else {
+} else {
     echo "私は紫垣ではありません。";
 }
+echo "\n";
 
+$total = 0;
 for($i = 1; $i <= 10000; $i++) {
-    echo $i;
+    $total += $i;
 }
+echo $total;
+echo "\n";
 
 $fruits = array("apple", "orange", "lemon", "grape", "strawberry");
 foreach($fruits as $fruit){
@@ -18,8 +22,9 @@ foreach($fruits as $fruit){
 
 $start = 1;
 $end = 100;
-for($i = $start; $i < $end; $i++){
+for($i = $start; $i <= $end; $i++){
     if($i % 5 == 0){
       echo $i;
+      echo "\n";
     }
 }
